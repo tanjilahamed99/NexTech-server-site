fe1796d13595c4be336eda3220807c2500e12581
Tanjil Ahamed <tanjil113355@gmail.com>
2023-12-13 15:02:24 +0600

    bookmark functonaliy added

--- src/api/Bookmark/postBookmark.js
+++ src/api/Bookmark/postBookmark.js
@@ -1,7 +1,20 @@
 const Bookmark = require("../../modal/Bookmark/Bookmark");
 const postBookmark = async (newBookmark) => {
-    const cursor = await Bookmark.create(newBookmark);
-    return cursor
+
+    const id = newBookmark.productsId
+    const email = newBookmark.email
+    const filter = {
+        productsId: id,
+        email: email
+    }
+    const exist = await Bookmark.findOne(filter)
+
+    if (!exist) {
+        const cursor = await Bookmark.create(newBookmark);
+        return cursor
+    }
+    return { Already: true }
+
 }
 
 module.exports = postBookmark
